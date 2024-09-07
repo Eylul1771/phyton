c601f5e452f8d5266050a95596c64152dfebf4f3
Eylül <eylul.demirdag@istun.edu.tr>
2024-09-07 15:56:05 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,36 +1,8 @@
-kitaplar = []
+import random
 
-def kitap_ekle(kitap_adı, yazar):
-    kitap = {"Kitap Adı": kitap_adı, "Yazar": yazar}
-    kitaplar.append(kitap)
-    print(f"'{kitap_adı}' kitabı eklendi.")
+# 1 ile 6 arasında rastgele bir sayı üret (zar atma)
+zar = random.randint(1, 6)
 
-def kitapları_göster():
-    if kitaplar:
-        print("Kütüphanedeki Kitaplar:")
-        for i, kitap in enumerate(kitaplar, 1):
-            print(f"{i}. {kitap['Kitap Adı']} - {kitap['Yazar']}")
-    else:
-        print("Kütüphanede hiç kitap yok.")
+# Sonucu ekrana yazdır
+print("Attığın zar:", zar)
 
-def kitap_sil(kitap_adı):
-    for kitap in kitaplar:
-        if kitap["Kitap Adı"] == kitap_adı:
-            kitaplar.remove(kitap)
-            print(f"'{kitap_adı}' kitabı silindi.")
-            return
-    print(f"'{kitap_adı}' kitabı bulunamadı.")
-
-def kütüphane_yönetimi():
-    while True:
-        print("\nKütüphane Yönetim Sistemi")
-        print("1. Kitap Ekle")
-        print("2. Kitapları Göster")
-        print("3. Kitap Sil")
-        print("4. Çıkış")
-        
-        seçim = input("Seçiminizi yapın: ")
-        
-        if seçim == '1':
-            kitap_adı = input("Kitap adı girin: ")
-            yazar = input("Yaz
