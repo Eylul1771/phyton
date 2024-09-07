a8ab75eb2bde8ff82a5b4c4f402f24d247b9fd7f
Eylül <eylul.demirdag@istun.edu.tr>
2024-09-07 16:46:57 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-import random
-
-# 1 ile 6 arasında rastgele bir sayı üret (zar atma)
-zar = random.randint(1, 6)
-
-# Sonucu ekrana yazdır
-print("Attığın zar:", zar)
-
+import random 
+seçenekler =["taş","kağıt","makas"]
+bilgisayar=random.choice(seçenekler)
+kullanıcı=input("Taş, kağıt veya makas").lower()
+print(f"Senin seçimin: {kullanıcı}")
+print(f"Bilgisayarın seçimi: {bilgisayar}")
+if kullanıcı ==bilgisayar :
+    print("berabere")
+    elif (kullanıcı == "taş" and bilgisayar == "makas") or \
+     (kullanıcı == "kağıt" and bilgisayar == "taş") or \
+     (kullanıcı == "makas" and bilgisayar == "kağıt"):
+    print(f"Sen kazandın! {kullanıcı} {bilgisayar}'yı yener.")
+else:
+    print(f"Bilgisayar kazandı! {bilgisayar} {kullanıcı}'yı yener.")
